94568c87a8a3e1cc05533a4e6461f06ca3c56f69
Mitanshu surana <55337570+mitanshusurana@users.noreply.github.com>
2025-11-05 16:03:10 +0530

    Add new allowed origin to CORS configuration

--- src/main/java/com/example/bullion/config/CorsConfig.java
+++ src/main/java/com/example/bullion/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**") // apply CORS to API paths
-                        .allowedOrigins("http://localhost:4200","http://localhost:8080") // Angular dev server
+                        .allowedOrigins("http://localhost:4200","http://localhost:8080","https://goldledgerbackend.onrender.com") // Angular dev server
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
@@ -22,3 +22,4 @@ public class CorsConfig {
         };
     }
 }
+
